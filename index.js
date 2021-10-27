7cbd15ea675eb539995f1705d23e1081a8436da5
Mohammad Islam <islam.mohammad.student@gmail.com>
2021-10-27 14:43:23 +0000

    finished

--- index.js
+++ index.js
@@ -1,30 +1,62 @@
 //1
-function betweenFiveAndTen(){
-  
+function betweenFiveAndTen(x){
+  if (x >= 5 && x <= 10) {
+      return true;
+  } else {
+      return false;
+  }
 }
 
 //2
-function betweenTenAndThirty(){
-  
+function betweenTenAndThirty(x){
+  if (x >= 10 && x <= 30) {
+      return true;
+  } else {
+      return false;
+  }
 }
 
 //3
-function betweenThirtyAndFifty(){
-  
+function betweenThirtyAndFifty(x){
+  if (x >= 30 && x <= 50) {
+      return true;
+  } else {
+      return false;
+  }
 }
 
-
 //4
-function betweenThirtyFiftySeventy(){
-  
+function betweenThirtyFiftySeventy(x){
+  if (x >= 0 && x <= 30 || x >= 50 && x <= 70) {
+      return true;
+  } else {
+      return false;
+  }
 }
 
 //5
-function betweenTwentyFiftySixty(){
-  
+function betweenTwentyFiftySixty(x){
+  if ((x > -1 && x < 21) || (x > 49 && x < 61)) {
+      return true;
+  } else {
+      return false;
+  }
 }
 
 //6
-function betweenTenTwentyThirty(){
-  
+function betweenTenTwentyThirty(x){
+  if ((x >= 0 && x <= 10) || (x >= 20 && x <= 30)) {
+      return true;
+  } else {
+      return false;
+  }
 }
+
+
+//calling functions:
+//console.log(betweenFiveAndTen(15));
+//console.log(betweenTenAndThirty(10));
+//console.log(betweenThirtyAndFifty(30));
+//console.log(betweenThirtyFiftySeventy(44));
+//console.log(betweenTwentyFiftySixty(55));
+//console.log(betweenTenTwentyThirty(22));
